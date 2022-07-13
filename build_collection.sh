01ea71de137339a4aa526dd01ba6e473bebbc573
adonisgarciac <agarciac@redhat.com>
2022-07-13 12:34:56 +0200

    change cicd to generate new release from tag

--- build_collection.sh
+++ build_collection.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+VERSION_RELEASE=` echo ${GITHUB_REF##*/} | grep -oP '(?<=Version_)[^ ]*'`
+
+sed -i "s/\$RELEASE/${VERSION_RELEASE}/g" galaxy.yml
 
 ansible-galaxy collection build --output-path ./releases
